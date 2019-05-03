6ec12e46c78bd0f04c966d9fbd1141c56aeba390
lu7did <lu7did@gmail.com>
2019-05-03 10:54:58 -0300

    misc

--- src/pixie/pixie.h
+++ src/pixie/pixie.h
@@ -18,15 +18,15 @@ void ModUpdate();
 
 //*-- Added by Lewis Loflin (LCD related)
 
-void lcd_init(void);
-void lcd_byte(int bits, int mode);
-void lcd_toggle_enable(int bits);
-void typeInt(int i);
-void typeFloat(float myFloat);
-void lcdLoc(int line);             //move cursor
-void ClrLcd(void);                 // clr LCD return home
-void typeln(const char *s);
-void typeChar(char val);
+//void lcd_init(void);
+//void lcd_byte(int bits, int mode);
+//void lcd_toggle_enable(int bits);
+//void typeInt(int i);
+//void typeFloat(float myFloat);
+//void lcdLoc(int line);             //move cursor
+//void ClrLcd(void);                 // clr LCD return home
+//void typeln(const char *s);
+//void typeChar(char val);
 void showFreq();                       //Prototype fur display used on callback
 void setDDSFrequency();
 
