9df597cf4b837d4f780b18c747c4ae24c773aef0
lu7did <lu7did@gmail.com>
2019-10-19 11:36:59 -0300

    usb

---
+++ bash/ssbZ.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+#*-----------------------------------------------------------------*
+#* ssb.sh
+#* Experimental SSB generator chain for FT8 transceiver
+#*
+#* ssb.sh FREQ [in Hz]
+#*-----------------------------------------------------------------*
+arecord -c1 -r48000 -D hw:CARD=Device,DEV=0 -fS16_LE - | csdr convert_i16_f \
+  | csdr fir_interpolate_cc 2 | csdr dsb_fc \
+  | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff  \
+  | sudo /home/pi/PixiePi/bin/ssbvox -i /dev/stdin -s 96000 -f 7070000 -t float
+
+
