d9039ba484a2021ec7357ddbf789b3e2d9282599
lu7did <lu7did@gmail.com>
2020-05-01 21:31:16 -0300

    Pi4D

--- bash/Pi4D.sh
+++ bash/Pi4D.sh
@@ -14,8 +14,9 @@ echo "Frequency defined: $1"
 
 socat -d -d pty,raw,echo=0,link=/tmp/ttyv0 TCP-LISTEN:8080 &
 PID=$!
+sudo rm -r /tmp/ptt_fifo
 echo "Pipe for /tmp/ttyv0 PID($PID)"
-arecord -c1 -r48000 -D hw:1 -fS16_LE - | sudo /home/pi/PixiePi/bin/Pi4D -i /dev/stdin -s 6000 -p /tmp/ttyv0 -f "$1" -a
+arecord -c1 -r48000 -D hw:1 -fS16_LE - | sudo /home/pi/PixiePi/bin/Pi4D -x -t 3  -p /tmp/ttyv0 -f "$1"
 echo "Removing /tmp/ttyv0 PI($PID)"
 sudo pkill socat
 /home/pi/PixiePi/bash/LCDoff.py
